97f95fec9830bd3eca44588653668c1b7ce640ce
dz24 <danielzh@stud.ntnu.no>
2023-01-20 13:53:27 +0100

    change

--- examples/internal/script.sh
+++ examples/internal/script.sh
@@ -4,7 +4,6 @@
 cp ../../infretis/
 
 cp ../../infretis/conv_inf_py.py  .
-cp ../../infretis/easy_swap.py    .
 cp ../../infretis/infretis.py     .
 cp ../../infretis/pattern.py      .
 cp ../../infretis/scheduler.py    .
@@ -15,5 +14,5 @@ python3 conv_inf_py.py # to conv infretis_data.txt to 00*/pathensemble.txt
 python3 pattern.py     # to create pattern
 
 rm -r 0* out.rst* pyretis.log* pyretis.restart*
-rm conv_inf_py.py easy_swap.py  infretis.py   
+rm conv_inf_py.py infretis.py   
 rm pattern.py scheduler.py pattern.txt infretis_data.txt
