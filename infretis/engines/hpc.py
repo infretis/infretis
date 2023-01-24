2a3fa26b3a3799ecdc33669e2731d87d4cc027e3
dz24 <danielzh@stud.ntnu.no>
2023-01-24 16:52:14 +0100

    update md and hpc

--- infretis/engines/hpc.py
+++ infretis/engines/hpc.py
@@ -1,2 +1,9 @@
+import os
 # something something hpc here.
 
+
+def hpc_checker()
+    if "SLURM_JOB_ID" in os.environ:
+        print("Running in Slurm")
+    else:
+        print("NOT running in Slurm")
