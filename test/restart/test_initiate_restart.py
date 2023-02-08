32f33538b7bcda9e51d6f44cbd0de397dff95f7a
dz24 <danielzh@stud.ntnu.no>
2023-02-08 13:29:41 +0100

    added/changed more sh wf unittests

--- test/restart/test_initiate_restart.py
+++ test/restart/test_initiate_restart.py
@@ -9,8 +9,11 @@ from dask.distributed import dask, Client, as_completed
 class test_repex_random(unittest.TestCase):
     
     def test_repex_random_1(self):
+
+        # create REPEX state with 2 workers and 5 ensembles
         state1 = REPEX_state(5, workers=2, minus=True)
 
+        # run global random gen
         for _ in range(10):
             np.random.rand()
         saved_rng1 = []
@@ -26,18 +29,8 @@ class test_repex_random(unittest.TestCase):
                 saved_rng2.append(np.random.rand())
         self.assertTrue(all([i == j for i, j in zip(saved_rng1, saved_rng2)]))
 
-
-
-        # for _ in range(10):
-        #     print(np.random.rand())
-
-        print('hihi')
-
-
         w_steal = dask.config.get('distributed.scheduler.work-stealing')
         self.assertFalse(dask.config.get('distributed.scheduler.work-stealing'))
-        # scheduler, futures = setup_dask(workers=workers)
-
 
 if __name__ == '__main__':  
     unittest.main()
