0a71246c12a204d1cf430aa35a17337f2cb4a6e1
dz24 <danielzh@stud.ntnu.no>
2023-02-10 13:35:42 +0100

    made pattern print more pretty

--- infretis/pattern.py
+++ infretis/pattern.py
@@ -1,5 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
+plt.rcParams.update({'font.size': 16})
+
 
 def pattern(inp, cap=250):
     a = np.loadtxt(inp)[:cap]
@@ -10,27 +12,54 @@ def pattern(inp, cap=250):
     workers = list(set(a[:, 3]))
     min_ens, max_ens = min(a[:,0]), max(a[:,0])
     c_dic = {0: 'r', 1: 'b', 2: 'g'}
-    dic = {int(worker): [] for worker in workers}
+    c_dic2 = {0: '#ffcccc', 1: '#ccccff', 2: '#cce5cc'}
+    dic = {int(worker): None  for worker in workers}
     label_workers = []
+
+    # plot worker end-> start |
+    for worker in workers:
+        x = np.array([(i) for i in a if i[3] == int(worker)])
+        time_ends = list(set([i[2] for i in x]))
+        enss = {time: [] for time in time_ends}
+        for time in time_ends:
+            for i in x:
+                if i[1] in time_ends:
+                    enss[i[1]].append(i[0])
+                if i[2] in time_ends:
+                    enss[i[2]].append(i[0])
+        for time in time_ends:
+            if len(set(enss[time])) > 1:
+                minmax = [min(enss[time]), max(enss[time])]
+                plt.plot([time]*2, minmax, '--',
+                         color=c_dic2[int(worker)], linewidth='2.')
+        dic[int(worker)] = time_ends
+
+    # scatter
+    for worker in workers:
+        for i in dic[int(worker)]:
+            enss = list(range(int(min_ens), int(max_ens)+1))
+            time = i
+            for row in a:
+                if row[1] < time < row[2] and worker != row[3]:
+                    enss.pop(enss.index(int(row[0])))
+            plt.scatter([time]*len(enss), enss,
+                        color=c_dic2[int(worker)], marker=r's')
+
+    # plot worker start -> end -
     for i in range(len(a)):
         worker = int(a[i][3])
         if worker not in label_workers:
-            plt.plot([a[i][1], a[i][2]], [a[i][0]]*2, color=c_dic[worker], marker="|", label=f'worker {worker}')
+            plt.plot([a[i][1], a[i][2]], [a[i][0]]*2, color=c_dic[worker],
+                     marker="|", label=f'worker {worker}', linewidth='2.')
             label_workers.append(worker)
         else: 
-            plt.plot([a[i][1], a[i][2]], [a[i][0]]*2, color=c_dic[worker], marker="|")
+            plt.plot([a[i][1], a[i][2]], [a[i][0]]*2, color=c_dic[worker],
+                     marker="|", linewidth='2.')
     
-    for worker in workers:
-        x = np.array([(i) for i in a if i[3] == int(worker)])
-        for i in range(len(x)):
-            if i + 1 < len(x) and x[i][0] != x[i+1][0]:
-                dic[int(worker)].append([[x[i][2]]*2, [x[i][0], x[i+1][0]]])
-        for i in dic[int(worker)]:
-            plt.plot(i[0], i[1], alpha=0.2, color=c_dic[int(worker)])
-    
-    plt.plot([3, max(a[:, 2])-3], [max_ens+3]*2, color='k', alpha=0.0)
-    plt.plot([3, max(a[:, 2])-3], [min_ens-3]*2, color='k', alpha=0.0)
-    plt.xlabel(r"Time [s]")
+    plt.xlabel(r"Time")
     plt.ylabel(r"Ensemble")
-    lgd = plt.legend(bbox_to_anchor=(1.04, 0.5), loc="center left", borderaxespad=0,  edgecolor='k', framealpha=1.0,)
+    plt.xticks([])
+    plt.yticks(list(range(int(min_ens), int(max_ens)+1)))
+    lgd = plt.legend(bbox_to_anchor=(1.04, 0.5), loc="center left",
+                     borderaxespad=0,  edgecolor='k', framealpha=1.0,)
     plt.savefig('pattern.pdf', bbox_extra_artists=(lgd,), bbox_inches='tight')
