a64590eedecca0e5b69cef954f460c021209a1e7
dz24 <danielzh@stud.ntnu.no>
2023-05-26 15:49:30 +0200

    that makes sense lol, because we removed path rand gen to only use ens rand gen.

--- test/examples/external/test_external_1.py
+++ test/examples/external/test_external_1.py
@@ -56,88 +56,88 @@ class test_infretisrun(unittest.TestCase):
             os.chdir(file_path)
         os.chdir(curr_path)
 
-#     def test_infretisrun_1worker_wf(self):
-#         # get path of where we run coverage unittest
-#         curr_path = pathlib.Path.cwd()
-# 
-#         # get path and cd into current file
-#         file_path = pathlib.Path(__file__).parent
-#         os.chdir(file_path)
-# 
-#         with tempfile.TemporaryDirectory(dir='./') as tempdir:
-#             # cd to tempdir
-#             os.chdir(tempdir)
-#             # copy files from template folder
-#             folder = 'data'
-#             for fil in FILES:
-#                 shutil.copy(f'../{folder}/{fil}', './')
-#             os.mkdir('trajs')
-#             copy_tree(f'../{folder}/trajs', './trajs')
-#             for ens in range(3):
-#                 copy_tree(f'./trajs/{ens}', f'./trajs/e{ens}')
-# 
-#             # run standard simulation start command
-#             os.system("sed -i -e '16 s/20/5/' infretis.toml >> out.txt")
-#             os.system("infretisrun -i infretis.toml >| out.txt")
-# 
-#             # compare files
-#             items0 = ['infretis_data.txt', 'restart.toml']
-#             items1 = ['wf5steps.txt', 'wf5steps.toml']
-#             for item0, item1 in zip(items0, items1):
-#                 istrue = filecmp.cmp(f'./{item0}', f'../{folder}/{item1}')
-#                 if not istrue:
-#                     print(f'./{item0}', f'../{folder}/{item1}')
-#                 self.assertTrue(istrue)
-# 
-#             # edit restart.toml by increasing steps from 5 to 10
-#             os.system("sed -i -e '24 s/5/10/' restart.toml >> out.txt")
-# 
-#             # restart standard simulation start command
-#             os.system("infretisrun -i restart.toml >> out.txt")
-#             # os.system("sed '55d' restart.toml >| restart0.toml")
-#             items0[1] = 'restart.toml'
-# 
-#             # compare files
-#             items2 = ['wf10steps.txt', 'wf10steps.toml']
-#             for item0, item2 in zip(items0, items2):
-#                 istrue = filecmp.cmp(f'./{item0}', f'../{folder}/{item2}')
-#                 if not istrue:
-#                     print(f'./{item0}', f'../{folder}/{item2}')
-#                 self.assertTrue(istrue)
-# 
-#             # # edit restart.toml by increasing steps from 10 to 15
-#             os.system("sed -i -e '24 s/10/15/' restart.toml >> out.txt")
-# 
-#             # restart standard simulation start command
-#             os.system("infretisrun -i restart.toml >> out.txt")
-#             os.system("sed '55d' restart.toml >| restart0.toml")
-# 
-#             # compare files
-#             items3 = ['wf15steps.txt', 'wf15steps.toml']
-#             for item0, item3 in zip(items0, items3):
-#                 istrue = filecmp.cmp(f'./{item0}', f'../{folder}/{item3}')
-#                 if not istrue:
-#                     print(f'./{item0}', f'../{folder}/{item3}')
-#                 self.assertTrue(istrue)
-# 
-#             # edit restart.toml by increasing steps from 15 to 20
-#             os.system("sed -i -e '24 s/15/20/' restart.toml >> out.txt")
-# 
-#             # restart standard simulation start command
-#             os.system("infretisrun -i restart.toml >> out.txt")
-#             os.system("sed '55d' restart.toml >| restart0.toml")
-# 
-#             # compare files
-#             items3 = ['wf20steps.txt', 'wf20steps.toml']
-#             for item0, item3 in zip(items0, items3):
-#                 istrue = filecmp.cmp(f'./{item0}', f'../{folder}/{item3}')
-#                 if not istrue:
-#                     print(f'./{item0}', f'../{folder}/{item3}')
-#                 self.assertTrue(istrue)
-# 
-#             # cd to previous, need to do this to delete tempdir
-#             os.chdir(file_path)
-#         os.chdir(curr_path)
+    def test_infretisrun_1worker_wf(self):
+        # get path of where we run coverage unittest
+        curr_path = pathlib.Path.cwd()
+
+        # get path and cd into current file
+        file_path = pathlib.Path(__file__).parent
+        os.chdir(file_path)
+
+        with tempfile.TemporaryDirectory(dir='./') as tempdir:
+            # cd to tempdir
+            os.chdir(tempdir)
+            # copy files from template folder
+            folder = 'data'
+            for fil in FILES:
+                shutil.copy(f'../{folder}/{fil}', './')
+            os.mkdir('trajs')
+            copy_tree(f'../{folder}/trajs', './trajs')
+            for ens in range(3):
+                copy_tree(f'./trajs/{ens}', f'./trajs/e{ens}')
+
+            # run standard simulation start command
+            os.system("sed -i -e '16 s/20/5/' infretis.toml >> out.txt")
+            os.system("infretisrun -i infretis.toml >| out.txt")
+
+            # compare files
+            items0 = ['infretis_data.txt', 'restart.toml']
+            items1 = ['wf5steps.txt', 'wf5steps.toml']
+            for item0, item1 in zip(items0, items1):
+                istrue = filecmp.cmp(f'./{item0}', f'../{folder}/{item1}')
+                if not istrue:
+                    print(f'./{item0}', f'../{folder}/{item1}')
+                self.assertTrue(istrue)
+
+            # edit restart.toml by increasing steps from 5 to 10
+            os.system("sed -i -e '24 s/5/10/' restart.toml >> out.txt")
+
+            # restart standard simulation start command
+            os.system("infretisrun -i restart.toml >> out.txt")
+            # os.system("sed '55d' restart.toml >| restart0.toml")
+            items0[1] = 'restart.toml'
+
+            # compare files
+            items2 = ['wf10steps.txt', 'wf10steps.toml']
+            for item0, item2 in zip(items0, items2):
+                istrue = filecmp.cmp(f'./{item0}', f'../{folder}/{item2}')
+                if not istrue:
+                    print(f'./{item0}', f'../{folder}/{item2}')
+                self.assertTrue(istrue)
+
+            # # edit restart.toml by increasing steps from 10 to 15
+            os.system("sed -i -e '24 s/10/15/' restart.toml >> out.txt")
+
+            # restart standard simulation start command
+            os.system("infretisrun -i restart.toml >> out.txt")
+            os.system("sed '55d' restart.toml >| restart0.toml")
+
+            # compare files
+            items3 = ['wf15steps.txt', 'wf15steps.toml']
+            for item0, item3 in zip(items0, items3):
+                istrue = filecmp.cmp(f'./{item0}', f'../{folder}/{item3}')
+                if not istrue:
+                    print(f'./{item0}', f'../{folder}/{item3}')
+                self.assertTrue(istrue)
+
+            # edit restart.toml by increasing steps from 15 to 20
+            os.system("sed -i -e '24 s/15/20/' restart.toml >> out.txt")
+
+            # restart standard simulation start command
+            os.system("infretisrun -i restart.toml >> out.txt")
+            os.system("sed '55d' restart.toml >| restart0.toml")
+
+            # compare files
+            items3 = ['wf20steps.txt', 'wf20steps.toml']
+            for item0, item3 in zip(items0, items3):
+                istrue = filecmp.cmp(f'./{item0}', f'../{folder}/{item3}')
+                if not istrue:
+                    print(f'./{item0}', f'../{folder}/{item3}')
+                self.assertTrue(istrue)
+
+            # cd to previous, need to do this to delete tempdir
+            os.chdir(file_path)
+        os.chdir(curr_path)
 
 #     def test_pick_lock_wf1(self):
 #         # get current path
