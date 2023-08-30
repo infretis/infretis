6be2570b905e5571ff44b69633def09f625d2fc1
Anders Lervik <andersle@gmail.com>
2023-08-30 08:43:40 +0200

    Removed a test-skip and added test for this part.

--- test/core/test_core.py
+++ test/core/test_core.py
@@ -1,4 +1,6 @@
 """Test methods from infretis.core.core"""
+import errno
+import os  # Used to simulate errors for make_dirs
 import pathlib
 
 import pytest
@@ -8,7 +10,13 @@ from infretis.core.core import make_dirs
 THIS_FILE = pathlib.Path(__file__).resolve()
 
 
-def test_make_dirs(tmp_path):
+def mock_mkdir(path, mode=0o777):
+    if path is not None:
+        raise PermissionError(errno.EPERM, "Permission denied")
+    return path, mode
+
+
+def test_make_dirs(tmp_path, monkeypatch):
     """Test that we can create directories."""
     # Test that we fail if we try to create a directory
     # with the same name as a file that already exists:
@@ -25,3 +33,12 @@ def test_make_dirs(tmp_path):
     msg = make_dirs(test_dir)
     assert "already exist" in msg
     assert f"{test_dir}" in msg
+    # Test that we fail with an error if we get another type of
+    # exception. To do this, we here monky patch the make directories
+    # method to just raise an error:
+    with monkeypatch.context() as monkey:
+        monkey.setattr(os, "mkdir", mock_mkdir)
+        with pytest.raises(PermissionError) as errorinfo:
+            msg = make_dirs(tmp_path / "some_directory_to_create")
+            assert msg is None
+            assert errorinfo.value.errno == errno.EPERM
