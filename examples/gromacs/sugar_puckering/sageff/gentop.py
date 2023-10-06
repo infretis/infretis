2c9174811bbbbf9dd25fe99c1059ad63498fcaa6
Lukas Baldauf <lukas.baldauf@ntnu.no>
2023-10-06 11:26:26 +0200

    Update gentop.py

--- examples/gromacs/sugar_puckering/sageff/gentop.py
+++ examples/gromacs/sugar_puckering/sageff/gentop.py
@@ -12,4 +12,14 @@ out = Interchange.from_smirnoff(force_field=sage, topology=topology)
 out.to_gro("mol.gro")
 out.to_top("mol.top")
 
-print("""Now add <#include "amber99.ff/tip3p.itp"> line to mol.top""")
+with open("gromacs_input/topol.top", "w") as writefile:
+    with open("mol.top") as readfile:
+        for line in readfile:
+            if "[ moleculetype ]" in line:
+                writefile.write("; Include tip3p water topology\n")
+                writefile.write('#include "amber99.ff/ffnonbonded.itp"\n')
+                writefile.write('#include "amber99.ff/tip3p.itp"\n\n')
+            # if '[ system ]' in line:
+            #    writefile.write('; Include water topology\n')
+            #    writefile.write('#include "amber99.ff/tip3p.itp"\n\n')
+            writefile.write(line)
