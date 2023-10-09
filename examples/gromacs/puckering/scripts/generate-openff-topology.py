158c02063c2333d3cc070f0b19c0220ce971e8f7
Lukas Baldauf <lukas.baldauf@ntnu.no>
2023-10-09 17:00:35 +0200

    Update some more paths

--- examples/gromacs/puckering/scripts/generate-openff-topology.py
+++ examples/gromacs/puckering/scripts/generate-openff-topology.py
@@ -10,10 +10,10 @@ topology.box_vectors = unit.Quantity([2.1, 2.1, 2.1], unit.nanometer)
 sage = ForceField("openff-2.1.0.offxml")
 out = Interchange.from_smirnoff(force_field=sage, topology=topology)
 out.to_gro("../gromacs_input/mol.gro")
-out.to_top("../gromacs_input/mol.top")
+out.to_top("../gromacs_input/mol.itp")
 
 with open("../gromacs_input/topol.top", "w") as writefile:
-    with open("mol.top") as readfile:
+    with open("../gromacs_input/mol.itp") as readfile:
         for line in readfile:
             if "[ moleculetype ]" in line:
                 writefile.write("; Include tip3p water topology\n")
