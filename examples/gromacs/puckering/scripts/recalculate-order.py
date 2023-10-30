8d91d59d9332434efdedbe95e1506076654d3ede
Lukas Baldauf <lukas.baldauf@ntnu.no>
2023-10-30 13:42:42 +0100

    Modify recalc order script

--- examples/gromacs/puckering/scripts/recalculate-order.py
+++ examples/gromacs/puckering/scripts/recalculate-order.py
@@ -15,7 +15,11 @@ parser.add_argument("-trr", help="The .trr trajectory file")
 parser.add_argument(
     "-toml", help="The .toml input file defining the orderparameter"
 )
-parser.add_argument("-out", help="The output file. Default: order-rec.txt")
+parser.add_argument(
+    "-out",
+    help="The output file. Default: order-rec.txt",
+    default="order-rec.txt",
+)
 
 args = parser.parse_args()
 
@@ -25,15 +29,15 @@ with open(args.toml, "rb") as toml_file:
     toml_dict = tomli.load(toml_file)
 
 orderparameter = create_orderparameter(toml_dict)
-interfaces = toml_dict["simulation"]["interfaces"]
+# interfaces = toml_dict["simulation"]["interfaces"]
 
 with open(args.out, "w") as writefile:
+    writefile.write("# step\ttheta\tphi\tQ\n")
     for i, frame in enumerate(traj):
         system = SimpleNamespace(
             pos=frame[1]["x"],
             box=np.diag(frame[1]["box"]),
         )
-        op = orderparameter.calculate(system)[:1]
-        op += interfaces
+        op = orderparameter.calculate(system)
         line = f"{i} " + " ".join([f"{opi}" for opi in op]) + "\n"
         writefile.write(line)
