720cc1604fd893c71a13b77c55a981aa6920010b
Lukas Baldauf <lukas.baldauf@ntnu.no>
2023-11-01 12:42:06 +0100

    Add check for theta angle

---
+++ examples/gromacs/puckering/scripts/check-indices.py
@@ -0,0 +1,34 @@
+import argparse
+import subprocess
+from types import SimpleNamespace
+
+import numpy as np
+
+from infretis.classes.orderparameter import Puckering
+
+parser = argparse.ArgumentParser(
+    description="Calculate the theta and phi angle for an \
+            .sdf file given a set of indices"
+)
+
+parser.add_argument("-sdf", help="The .sdf file of your molecule")
+parser.add_argument(
+    "-idx", help="The ordered indices of your molecule", type=list, nargs="+"
+)
+
+args = parser.parse_args()
+
+orderparameter = Puckering(index=[int(i[0]) for i in args.idx])
+
+subprocess.run(f"obabel -isdf {args.sdf} -oxyz -O .temp.xyz", shell=True)
+x = np.loadtxt(".temp.xyz", skiprows=2, usecols=[1, 2, 3])
+subprocess.run("rm .temp.xyz", shell=True)
+
+system = SimpleNamespace(
+    pos=x,
+    box=[np.inf, np.inf, np.inf],
+)
+op = orderparameter.calculate(system)
+
+print(f"\nTheta = {op[0]:.3f} degrees")
+print(f"Phi = {op[1]:.3f} degrees")
