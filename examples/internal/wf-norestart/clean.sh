2c04df344e7405a55f8e4b02481bb2cff3b09145
dz24 <danielzh@stud.ntnu.no>
2023-11-01 18:01:53 +0100

    delete internal, make turtlemd workable and wf, change log structure

--- examples/internal/wf-norestart/clean.sh
+++
@@ -1,5 +0,0 @@
-# this deletes everything temp
-rm -r 0* out.rst* pyretis.log* pyretis.restart* *log
-rm pattern.txt infretis_data.txt restart.toml
-rm initial.xyz retis.rst infretis.restart out.txt
-rm -r trajs
