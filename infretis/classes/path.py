d5420ccb48d923f8ff5e9046f0fe3b8f3b14bc18
Anders Lervik <andersle@gmail.com>
2023-12-04 16:39:01 +0100

    Update also for docstrings.

--- infretis/classes/path.py
+++ infretis/classes/path.py
@@ -28,7 +28,7 @@ class Path:
         """Initiate Path class."""
         self.maxlen = maxlen
         self.status: str | None = None
-        self.generated = None
+        self.generated: tuple[str, None, None, None] | None = None
         self.path_number = None
         self.weights = None
         self.phasepoints: list[System] = []
@@ -82,18 +82,13 @@ class Path:
         The end point is either to the left of the `left` interface or
         to the right of the `right` interface, or somewhere in between.
 
-        Parameters
-        ----------
-        left : float
-            The left interface.
-        right : float, optional
-            The right interface, equal to left if not specified.
+        Args:
+            left: The left interface.
+            right: The right interface, equal to left if not specified.
 
-        Returns
-        -------
-        out : string
-            A string representing where the end point is ('L' - left,
-            'R' - right or None).
+        Returns:
+            out: A string representing where the end point is ('L' - left,
+                'R' - right or None).
 
         """
         if right is None:
@@ -117,18 +112,13 @@ class Path:
         The start point is either to the left of the `left` interface or
         to the right of the `right` interface.
 
-        Parameters
-        ----------
-        left : float
-            The left interface.
-        right : float, optional
-            The right interface, equal to left if not specified.
+        Args:
+            left: The left interface.
+            right: The right interface, equal to left if not specified.
 
-        Returns
-        -------
-        out : string
-            A string representing where the start point is ('L' - left,
-            'R' - right or None).
+        Returns:
+            out: A string representing where the start point is ('L' - left,
+                'R' - right or None).
 
         """
         if right is None:
@@ -151,16 +141,8 @@ class Path:
         logger.debug(f"Selected point with orderp {order}")
         return self.phasepoints[idx], idx
 
-    def append(self, phasepoint):
-        """Append a new phase point to the path.
-
-        Parameters
-        ----------
-        out : object like :py:class:`.System`
-            The system information we add to the path.
-
-        """
-        # ##maxlen adjust
+    def append(self, phasepoint) -> bool:
+        """Append a new phase point to the path."""
         if self.maxlen is None or self.length < self.maxlen:
             self.phasepoints.append(phasepoint)
             return True
@@ -172,15 +154,12 @@ class Path:
     ) -> dict[str, Any]:
         """Return information about the path.
 
-        This information can be stored in a object like
-        :py:class:`.PathEnsemble`.
+        Args:
+            status: The current status of the path.
+            interfaces:: The interfaces for the simulation.
 
-        Parameters
-        ----------
-        status : string
-            This represents the current status of the path.
-        interfaces : list
-            These are just the interfaces we are currently considering.
+        Returns:
+            path_info: A dict with information about the path.
 
         """
         path_info: dict[str, Any] = {
@@ -219,19 +198,13 @@ class Path:
         return self.ordermax[0] > target_interface
 
     def __iadd__(self, other: Path) -> Path:
-        """Add path data to a path from another path, i.e. ``self += other``.
+        """Append phase points from another path, `self += other`.
 
-        This will simply append the phase points from `other`.
+        Args:
+            other: The object to add path data from.
 
-        Parameters
-        ----------
-        other : object of type `Path`
-            The object to add path data from.
-
-        Returns
-        -------
-        self : object of type `Path`
-            The updated path object.
+        Returns:
+            self: The updated path object.
 
         """
         for phasepoint in other.phasepoints:
@@ -353,21 +326,17 @@ class Path:
     ):  # TODO: Can this be removed?
         """Re-order the phase points according to the given key.
 
-        Parameters
-        ----------
-        key : string
-            The attribute we will sort according to.
-        reverse : boolean, optional
-            If this is False, the sorting is from big to small.
+        Args:
+            key: The attribute we will sort according to.
+            reverse: If this is False, the sorting is from big to small.
 
-        Yields
-        ------
-        out : object like :py:class:`.System`
+        Yields:
             The ordered phase points from the path.
 
         """
         if key in ("ekin", "vpot"):
-            sort_after = [getattr(i.particles, key) for i in self.phasepoints]
+            # TODO: Particles are gone, remove this method
+            sort_after = [getattr(i, key)[0] for i in self.phasepoints]
         elif key == "order":
             sort_after = [getattr(i, key)[0] for i in self.phasepoints]
         else:
@@ -386,12 +355,9 @@ class Path:
         read from external engines and returned as a list of
         floats.
 
-        Parameters
-        ----------
-        ekin : list of floats
-            The kinetic energies to set.
-        vpot : list of floats
-            The potential energies to set.
+        Args:
+            ekin : The kinetic energies to set.
+            vpot : The potential energies to set.
 
         """
         if len(ekin) != len(vpot):
@@ -443,31 +409,28 @@ def paste_paths(
 
     - `path_back` must be iterated in reverse (it is assumed to be a
       backward trajectory).
-    - we may have to remove one point in `path2` (if the paths overlap).
+    - we may have to remove one point in `path_forw` (if the paths overlap).
 
-    Parameters
-    ----------
-    path_back : object like :py:class:`.PathBase`
-        This is the backward trajectory.
-    path_forw : object like :py:class:`.PathBase`
-        This is the forward trajectory.
-    overlap : boolean, optional
-        If True, `path_back` and `path_forw` have a common
-        starting-point, that is, the first point in `path_forw` is
-        identical to the first point in `path_back`. In time-space, this
-        means that the *first* point in `path_forw` is identical to the
-        *last* point in `path_back` (the backward and forward path
-        started at the same location in space).
-    maxlen : float, optional
-        This is the maximum length for the new path. If it's not given,
-        it will just be set to the largest of the `maxlen` of the two
-        given paths.
+    Args:
+        path_back: The backward trajectory.
+        path_forw: The forward trajectory.
+        overlap: If True, `path_back` and `path_forw` have a common
+            starting-point; the first point in `path_forw` is
+            identical to the first point in `path_back`. In time-space, this
+            means that the *first* point in `path_forw` is identical to the
+            *last* point in `path_back` (the backward and forward path
+            started at the same location in space).
+        maxlen: This is the maximum length for the new path.
+            If it's not given, it will just be set to the largest of
+            the `maxlen` of the two given paths.
 
-    Note
-    ----
-    Some information about the path will not be set here. This must be
-    set elsewhere. This includes how the path was generated
-    (`path.generated`) and the status of the path (`path.status`).
+    Returns:
+        new_path: The resulting path from the merge.
+
+    Note:
+        Some information about the path will not be set here. This must be
+        set elsewhere. This includes how the path was generated
+        (`path.generated`) and the status of the path (`path.status`).
 
     """
     if maxlen is None:
@@ -503,8 +466,8 @@ def paste_paths(
     return new_path
 
 
-def load_trajtxt(dirname: str):  # TODO: CAN THIS BE REMOVED?
-    """Load traj_txt."""
+def load_trajtxt(dirname: str) -> dict[str, Any]:  # TODO: CAN THIS BE REMOVED?
+    """Load the information in traj.txt."""
     traj_file_name = os.path.join(dirname, "traj.txt")
     with PathExtFile(traj_file_name, "r") as trajfile:
         # Just get the first trajectory:
@@ -521,7 +484,9 @@ def load_trajtxt(dirname: str):  # TODO: CAN THIS BE REMOVED?
         return traj
 
 
-def load_ordertxt(dirname: str):  # TODO: CAN THIS BE REMOVED?
+def load_ordertxt(
+    dirname: str,
+) -> dict[str, np.ndarray]:  # TODO: CAN THIS BE REMOVED?
     """Load order_txt."""
     order_file_name = os.path.join(dirname, "order.txt")
     with OrderPathFile(order_file_name, "r") as orderfile:
@@ -611,7 +576,7 @@ def _check_path(
     return accept, status
 
 
-def _load_energies_for_path(path, dirname):
+def _load_energies_for_path(path: Path, dirname: str):
     """Load energy data for a path.
 
     Parameters
@@ -638,7 +603,7 @@ def _load_energies_for_path(path, dirname):
         pass
 
 
-def load_paths_from_disk(config):
+def load_paths_from_disk(config: dict[str, Any]) -> list[Path]:
     """Load paths from disk."""
     load_dir = config["simulation"]["load_dir"]
     paths = []
