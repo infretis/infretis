4b4a345420bbda81a6f2be2e9f269cef4fff45fa
Lukas Baldauf <lukas.baldauf@ntnu.no>
2024-01-04 15:53:30 +0100

    Update test_velocity_functions.py

--- test/engines/test_velocity_functions.py
+++ test/engines/test_velocity_functions.py
@@ -11,27 +11,43 @@ from infretis.classes.system import System
 HERE = pathlib.Path(__file__).resolve().parent
 
 
-def return_lammps_engine() -> LAMMPSEngine:
+def maxwell_vels(mass, vel, beta):
+    out = (mass * beta / (2 * np.pi)) ** (1 / 2) * np.exp(
+        -mass * vel**2 * beta / 2
+    )
+    return out
+
+
+def return_lammps_engine():
     """Set up a lammps engine for the H2 system."""
     lammps_input_path = HERE / "../../examples/lammps/H2/lammps_input"
-    temperature = 300
-    engine = LAMMPSEngine(
-        "foobar", lammps_input_path.resolve(), -1, -1, temperature
-    )
+    engine = LAMMPSEngine("foobar", lammps_input_path.resolve(), -1, -1)
+    print(engine.input_path)
     engine.rgen = np.random.default_rng()
     return engine
 
 
-def return_gromacs_engine() -> GromacsEngine:
+def return_gromacs_engine():
     """Set up a gromacs engine for the H2 system."""
-    gromacs_input_path = HERE / "../../examples/gromacs/H2/gromacs_input/"
-    # set`gmx = echo` here because __init__ calls it
-    engine = GromacsEngine("echo", "bar", gromacs_input_path, -1, -1)
+    gromacs_input_path = HERE / "../../examples/gromacs/H2/gromacs_input"
+    # set`gmx = echo` here because __init__ calls `gmx` with subprocess
+    engine = GromacsEngine(
+        "echo", "foobar", gromacs_input_path.resolve(), -1, -1
+    )
+    # engine.beta = 1/(300*0.0083144621)
+    engine.rgen = np.random.default_rng()
     return engine
 
 
-@pytest.mark.parametrize("engine", [return_lammps_engine()])
-def test_modify_velocities(tmp_path: pathlib.PosixPath, engine) -> None:
+return_engines = [return_lammps_engine(), return_gromacs_engine()]
+
+engine_classes = [LAMMPSEngine, GromacsEngine]
+
+
+@pytest.mark.parametrize(
+    "engine", [return_lammps_engine(), return_gromacs_engine()]
+)
+def test_modify_velocities(tmp_path, engine):
     """Check that we can modify the velocities with an engine."""
     # folder we wil run from
     folder = tmp_path / "temp"
@@ -40,11 +56,18 @@ def test_modify_velocities(tmp_path: pathlib.PosixPath, engine) -> None:
     engine.exe_dir = folder
 
     system = System()
-    system.set_pos((initial_conf, 0))
-    engine.modify_velocities(system, {})
+    system.set_pos((str(initial_conf.resolve()), 0))
+    vel_settings = {
+        "zero_momentum": False,
+        "infretis_genvel": True,  # generate velocities internally for gromacs
+        "temperature": 300,
+        "mass": [1.008, 1.008],
+    }
 
-    # check if the genvel is written
+    engine.modify_velocities(system, vel_settings)
+
+    # check if the genvel file is written
     genvel_conf = folder / f"genvel.{engine.ext}"
     assert genvel_conf.is_file()
-    # since we generated velocities we should not have 0 kinetic energy
+    # we generated velocities, so we should have non-zero kinetic energy
     assert system.ekin != 0
