e887fafda58cbfc3505d46172373227334586533
dz24 <55857383+dz24@users.noreply.github.com>
2024-04-19 15:01:35 +0200

    Fix multiw restart (#93)
    * ensure workers to restart their previous tasks
    * ensure all worker logs gets initiated
    * add corrected test sim data
    
    ---------
    
    Co-authored-by: dz24 <daniel.t.zhang@ntnu.no>

--- test/simulations/test_run_infretis.py
+++ test/simulations/test_run_infretis.py
@@ -3,10 +3,31 @@ import filecmp
 import os
 import shutil
 from pathlib import PosixPath
+from subprocess import STDOUT, check_output
 
 import pytest
 import tomli
 import tomli_w
+
+
+def read_simlogw(inp, workers=4, restart=False):
+    winfo = [None for i in range(workers)]
+    restartcnt = 0
+    with open(inp) as f:
+        for line in f:
+            if "shooting" in line:
+                rip = line.rstrip().split()
+                if len(rip) == 12:
+                    worker, ens_path = int(rip[-1]), [rip[5], rip[8]]
+                else:
+                    worker, ens_path = int(rip[-1]), [rip[5], rip[9]]
+                winfo[worker] = ens_path
+            if restart:
+                if f"submit worker {workers-1} END" in line:
+                    restartcnt += 1
+                if restartcnt == 2:
+                    break
+    return sorted([f"{i[0]}-{i[1]}" for i in winfo])
 
 
 def change_toml_steps(inp, steps):
@@ -71,3 +92,60 @@ def test_run_airetis_wf(tmp_path: PosixPath) -> None:
     # nb: technically num_files == 24, but due to restarts pn_olds get reset.
     num_files = len(os.listdir("load"))
     assert num_files < 40
+
+
+@pytest.mark.heavy
+def test_restart_multiple_w(tmp_path: PosixPath) -> None:
+    """Check that restarted workers continue the same tasks pre-restart."""
+    folder = tmp_path / "temp"
+    folder.mkdir()
+    basepath = os.path.dirname(__file__)
+    load_dir = os.path.join(
+        basepath, "../../examples/turtlemd/double_well/load_copy"
+    )
+    toml_dir = os.path.join(basepath, "data/wf.toml")
+    # copy files from template folder
+    shutil.copytree(load_dir, str(folder) + "/load")
+    shutil.copy(toml_dir, str(folder) + "/infretis.toml")
+    os.chdir(folder)
+
+    workers = 4
+    with open("infretis.toml", mode="rb") as f:
+        config = tomli.load(f)
+        config["simulation"]["steps"] = 1000000
+        config["dask"]["workers"] = workers
+    with open("infretis.toml", "wb") as f:
+        tomli_w.dump(config, f)
+
+    # WIP: can possibly decrease timeout s. if github pipeline allows it.
+    # start 4w 5s simulation
+    try:
+        check_output(
+            ["infretisrun", "-i", "infretis.toml"], stderr=STDOUT, timeout=3
+        )
+    except Exception:
+        pass
+    winfo1 = read_simlogw("sim.log", workers)
+
+    with open("restart.toml", mode="rb") as f:
+        config = tomli.load(f)
+        cstep = config["current"]["cstep"]
+    # arbitrary small number
+    assert cstep > 10
+
+    # restart 4w 5s simulation
+    try:
+        check_output(
+            ["infretisrun", "-i", "restart.toml"], stderr=STDOUT, timeout=3
+        )
+    except Exception:
+        pass
+
+    winfo2 = read_simlogw("sim.log", workers, restart=True)
+    for work1, work2 in zip(winfo1, winfo2):
+        assert work1 == work2
+
+    # check that we have 4 worker.logs
+    dirs = os.listdir(".")
+    for i in range(workers):
+        assert f"worker{i}.log" in dirs
