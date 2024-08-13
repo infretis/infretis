9e3f5e63acf137ca5026c78b86d9cf8a283805eb
Lucas Esclapez <l.esclapez@esciencecenter.nl>
2024-08-13 12:21:59 +0200

    Update scheduler, almost back to Dask-based version.

--- infretis/scheduler.py
+++ infretis/scheduler.py
@@ -16,35 +16,22 @@ def scheduler(config):
         md_items = state.prep_md_items(md_items)
 
         # submit job to scheduler
-        futures.append(runner.submit_work(md_items))
+        futures.add(runner.submit_work(md_items))
 
     # main step loop
     while state.loop():
+        # Get futures as they are completed
+        future = futures.as_completed()
+        if future:
+            md_items = state.treat_output(future.result())
 
-        one_future_done = False
+        # submit new job
+        if state.cstep + state.workers <= state.tsteps:
+            # chose ens and path for the next job
+            md_items = state.prep_md_items(md_items)
 
-        # At each loop iteration, check the status of futures
-        while not one_future_done:
-            for fut in futures:
-                if fut.done():
-                    print(" pin: {}, cstep: {}".format(md_items["pin"], state.cstep))
-                    # get and treat worker output
-                    md_items = state.treat_output(fut.result())
-
-                    # submit new job:
-                    if state.cstep + state.workers <= state.tsteps:
-                        # chose ens and path for the next job
-                        md_items = state.prep_md_items(md_items)
-
-                        # submit job to scheduler
-                        futures.append(runner.submit_work(md_items))
-                        futures.remove(fut)
-                    one_future_done = True
-                    break
-            #time.sleep(0.1)
-
-        #print("State loop")
-
+            # submit job to scheduler
+            futures.add(runner.submit_work(md_items))
 
     # end client
     runner.stop()
