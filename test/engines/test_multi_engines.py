e631134bcd0373b36ba21d26a45d0d1ea9080e8b
Lukas Baldauf <lukas.baldauf@ntnu.no>
2024-09-20 17:14:52 +0200

    add smiple quantis test

--- test/engines/test_multi_engines.py
+++ test/engines/test_multi_engines.py
@@ -2,6 +2,8 @@
 
 import pathlib
 
+import numpy as np
+
 from infretis.classes.engines.factory import create_engines
 from infretis.setup import setup_config
 
@@ -29,3 +31,18 @@ def test_quantis_potentials():
         engines["engine0"][1][0].potential[0].desc
         != engines["engine"][1][0].potential[0].desc
     )
+
+
+def test_potential_curves():
+    engines = get_quantis_engines()
+    e0 = engines["engine0"][1][0]
+    e1 = engines["engine"][1][0]
+    x = np.linspace(0.3, 1.3, 50)
+    e1.system.particles.pos *= 0
+    en0 = []
+    en1 = []
+    for i, xi in enumerate(x):
+        e1.system.particles.pos[0, 0] = xi
+        en0.append(e0.potential[0].potential(e1.system))
+        en1.append(e1.potential[0].potential(e1.system))
+    assert not np.allclose(en0, en1)
