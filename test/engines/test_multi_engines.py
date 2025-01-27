2527dd1ad86d083c2b73b83ae75d5030dd49de3c
dz24 <55857383+dz24@users.noreply.github.com>
2025-01-27 21:34:32 +0900

    update dep and pipeline (#145)
    * update dep and pipeline
    
    * update pytoml
    
    * fix docstyle
    
    * try pass
    
    * update lint
    
    * add turtle to lint runner
    
    * update coverage
    
    * degrade py version for coverage pass maybe
    
    * bring poetry back
    
    * revert version
    
    * rm poetry from pytest run
    
    * remove potery again
    
    * add missing package
    
    * change name
    
    * add init files and e install
    
    * add string to init
    
    * change to add ams reqs
    
    ---------
    
    Co-authored-by: dz24 <daniel.t.zhang@ntnu.no>

--- test/engines/test_multi_engines.py
+++ test/engines/test_multi_engines.py
@@ -1,6 +1,7 @@
 """Test what?."""
 
 import pathlib
+import os
 
 import numpy as np
 
@@ -10,20 +11,21 @@ from infretis.setup import setup_config
 HERE = pathlib.Path(__file__).resolve().parent
 
 
-def get_quantis_engines():
+def get_quantis_engines(tmp_path: pathlib.PosixPath):
     """Set up a turtlemd engine for the H2 system where
     [N+] potential is Lennard-Jones and [0-] is PairDoubleWell.
     """
     input_path = HERE / "../../examples/turtlemd/H2_quantis/"
     toml_file = input_path / "infretis.toml"
+    os.chdir(tmp_path)
     config = setup_config(toml_file)
     engines = create_engines(config)
     return engines
 
 
-def test_quantis_potentials():
+def test_quantis_potentials(tmp_path: pathlib.PosixPath):
     """Test that we actually create two different potentials with quantis."""
-    engines, _ = get_quantis_engines()
+    engines, _ = get_quantis_engines(tmp_path)
     # We should have 2 engines
     assert len(engines) == 2
     # The two engines should have different potentials
@@ -33,8 +35,8 @@ def test_quantis_potentials():
     )
 
 
-def test_potential_curves():
-    engines, _ = get_quantis_engines()
+def test_potential_curves(tmp_path: pathlib.PosixPath):
+    engines, _ = get_quantis_engines(tmp_path)
     e0 = engines["engine0"][0]
     e1 = engines["engine"][0]
     x = np.linspace(0.3, 1.3, 50)
